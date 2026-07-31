dac406fa40bd55757ccea982fe59ce2e8dffb772
Wearily <parsapournabi1382@gmail.com>
2026-07-31 20:56:13 +0330

    add general DEVELOPMENT flag and _DEVELOPMENT usage in example.

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,11 +3,21 @@
 
 #include <Module/ModuleTest.h>
 
+inline bool constexpr isDevelopment()
+{
+#ifdef Module_DEVELOPMENT
+    return true;
+#else
+    return false;
+#endif
+}
+
 int main(int argc, char** argv)
 {
     QCoreApplication app(argc, argv);
     // std::cout << "Example for " << "@MODULE_NAME@" << std::endl;
     PrintableModule pm;
     pm.smartPrint("Hello world this is test from Example Module");
+    std::cout << "Development Mode: " <<  isDevelopment() << std::endl;
     return 0;
 }
