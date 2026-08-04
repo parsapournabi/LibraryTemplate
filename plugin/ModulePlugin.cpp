ae7b3f0ff45fbb5b5b6d99cfaf18e7801fc22b20
Wearily <parsapournabi1382@gmail.com>
2026-08-04 17:02:58 +0330

    add template Plugin class

---
+++ plugin/ModulePlugin.cpp
@@ -0,0 +1,13 @@
+#include "ModulePlugin.h"
+
+// Include MainApplication Module
+
+void ModuleQuickPlugin::registerTypes(const char* uri)
+{
+    // Register QML Types of Module
+}
+
+void ModuleQuickPlugin::initializeEngine(QQmlEngine* engine, const char* uri)
+{
+    // Add ImportPaths and other engine options
+}
